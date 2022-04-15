351e3dad6e4c92ec0711efd9e5c9e8f4543637c3
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 11:02:20 -0700

    Send dog data from fetch down to Bar as prop.

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,19 @@ const API = "http://localhost:3001/pups";
 function App() {
   // Store filter state at App level
   const [filterState, setFilterState] = useState(false);
+  const [dogArrayState, setDogArrayState] = useState([]);
 
   // Get dog info from API upon page load
   useEffect(() => {
     fetch(API)
       .then(r => r.json())
-      .then(data => console.log(data));
+      .then(setDogArrayState);
   }, []);
 
   return (
     <div className="App">
       <Filter filterState={filterState} setFilterState={setFilterState} />
-      <Bar />
+      <Bar dogArray={dogArrayState} />
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
         <div id="dog-info"></div>
---
+++ src/components/Bar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bar({ dogArray }) {
+  console.log(dogArray);
+
+  return <div id="dog-bar">
+    
+  </div>;
+}
+
+export default Bar;
