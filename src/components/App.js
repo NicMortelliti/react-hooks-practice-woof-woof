3dfcaea88689181f7b4e359d6683edcbf961b97f
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 10:31:27 -0700

    Move filter button to separate Filter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import Filter from "./Filter";
 
 function App() {
   return (
     <div className="App">
-      <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
-      </div>
+      <Filter />
       <div id="dog-bar"></div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
