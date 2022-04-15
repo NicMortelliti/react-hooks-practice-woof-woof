572d1ff7c1f68d672b2d51d926b2c05d2cd97f3d
NicMortelliti <npmortelliti@gmail.com>
2022-04-15 10:57:38 -0700

    Fetch dog data from API. Console log results

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./Filter";
 import Bar from "./Bar";
+
+const API = "http://localhost:3001/pups";
 
 function App() {
   // Store filter state at App level
   const [filterState, setFilterState] = useState(false);
+
+  // Get dog info from API upon page load
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <div className="App">
